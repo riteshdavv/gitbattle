86b95613aaaaa6dc7bb95484a9acaa5a502e58be
Ritesh Singh <ritesh.davv@gmail.com>
2025-01-04 22:27:34 +0530

    Add async jwt and session in callbacks

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -11,5 +11,19 @@ export const NEXT_AUTH = {
             authorization: { params: { scope: "read:user user:email" } }
         })
     ],
-    
+    callbacks: {
+        async jwt({ token, account }: any) {
+            if (account) {
+              token.accessToken = account.access_token; // Store access token
+              token.githubUsername = account.providerAccountId; // Store GitHub username
+            }
+            return token;
+        },
+        async session({ session, token }: any) {
+            session.accessToken = token.accessToken; // Add access token to session
+            session.githubUsername = token.githubUsername; // Add username to session
+            return session;
+        },
+        
+    }
 }
